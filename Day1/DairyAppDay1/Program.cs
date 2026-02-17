ed87d71d57e055ff3fd6448ba715bce16c4ebfc8
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2026-02-17 23:29:13 +0530

    --

---
+++ Day1/DairyAppDay1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter Farmer Name: ");
+string name = Console.ReadLine();
+
+Console.Write("Enter Milk Quantity: ");
+double qty = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Rate per Liter: ");
+double rate = Convert.ToDouble(Console.ReadLine());
+
+double total = qty * rate;
+
+Console.WriteLine("Farmer: " + name);
+Console.WriteLine("Total Amount: " + total);
+
