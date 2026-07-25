2086fe5fd577b9b839e03fdca347b20eabc69b9a
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2026-07-25 22:36:06 +0530

    Implement Farmer CRUD create and get all APIs

--- SmartDairyManagementSystem/SmartDairy.Api/Controllers/FarmerController.cs
+++ SmartDairyManagementSystem/SmartDairy.Api/Controllers/FarmerController.cs
@@ -1,6 +1,38 @@
-﻿namespace SmartDairy.Api.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartDairy.Application.Features.Farmers.DTOs;
+using SmartDairy.Application.Features.Farmers.Interfaces;
+
+namespace SmartDairy.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class FarmerController : ControllerBase
 {
-    public class FarmerController
+    private readonly IFarmerService _farmerService;
+
+    public FarmerController(IFarmerService farmerService)
     {
+        _farmerService = farmerService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddFarmer(CreateFarmerRequest request)
+    {
+        var result = await _farmerService.AddFarmerAsync(request);
+
+        if (!result.Success)
+            return BadRequest(result);
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllFarmers()
+    {
+        var result = await _farmerService.GetAllFarmersAsync();
+
+        return Ok(result);
     }
 }
